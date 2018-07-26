f93d83bef53b0c898b2a3ee48ed1e025168c0c33
Shawn Allen <shawn.allen@github.com>
2018-07-26 01:32:03 -0700

    add lots more props

--- src/generate.js
+++ src/generate.js
@@ -1,12 +1,35 @@
 const postcss = require('postcss')
 const {get} = require('dotmap')
-const styled = require('styled-system')
+const styledSystem = require('styled-system')
 
 const systemProps = [
-  {name: 'space'},
-  {name: 'fontSize', prefix: 'f'},
-  {name: 'color', responsive: false}
-].map(prop => ({...prop, func: styled[prop.name]}))
+  // color
+  {name: 'bgColor'},
+  {name: 'textColor'},
+  // borders
+  {name: 'border'},
+  {name: 'borderRadius', prefix: 'round', responsive: true},
+  // typography
+  {name: 'fontSize', prefix: 'f', responsive: true},
+  {name: 'fontFamily', prefix: 'font'},
+  {name: 'lineHeight', prefix: 'lh', responsive: true},
+  {name: 'fontWeight', prefix: 'text'},
+  {name: 'textStyle', prefix: 'text'},
+  // layout, whitespace
+  {name: 'space', responsive: true},
+  {name: 'display', prefix: 'd', responsive: true, scale: mapify([
+    'block',
+    'flex',
+    'inline-flex',
+    'inline',
+    'inline-block',
+    'none',
+    'table',
+    'table-cell'
+  ])},
+  // etc.
+  {name: 'boxShadow'},
+].map(prop => ({...prop, func: styledSystem[prop.name]}))
 
 // see: styled-system/src/#TODO
 const defaultBreakpoints = [40, 52, 64,].map(n => n + 'em')
@@ -47,6 +70,9 @@ module.exports = function generateCSS(theme = defaultTheme) {
   const root = postcss.root()
 
   for (const systemProp of systemProps) {
+    if (!systemProp.func) {
+      throw new Error(`No such style prop: "${systemProp.name}"`)
+    }
     const rules = generateRulesForStyle(systemProp, theme, breakpoints)
     if (rules.length === 0) {
       console.warn('no rules for prop: %s', systemProp.name)
@@ -68,13 +94,15 @@ function generateRulesForStyle(systemProp, theme, breaks) {
   const {
     func,
     prefix,
-    responsive = true
+    scale: overrideScale,
+    responsive = false
   } = systemProp
 
   const breakpoints = responsive ? breaks : [null]
 
   const metas = Object.keys(func.propTypes)
     .map(prop => func.propTypes[prop].meta)
+    .filter(meta => meta)
 
   const rules = []
   for (const meta of metas) {
@@ -85,7 +113,10 @@ function generateRulesForStyle(systemProp, theme, breaks) {
       themeKey
     } = meta
 
-    const scale = get(theme, themeKey) || getDefaultScale(prop, cssProperty) || defaultScale
+    const scale = overrideScale ||
+      get(theme, themeKey) ||
+      getDefaultScale(prop, cssProperty) ||
+      defaultScale
 
     for (const brk of breakpoints) {
       const {
@@ -97,32 +128,27 @@ function generateRulesForStyle(systemProp, theme, breaks) {
         ? rule => atRule.append(rule)
         : rule => rules.push(rule)
 
-      for (const [key, scaleValue] of Object.entries(scale)) {
-        if (scaleValue && typeof scaleValue === 'object') {
-          continue
-        }
-        const selector = getSelector({
-          prop: prefix || prop,
-          breakpoint,
-          key,
-          value: scaleValue
-        })
-        const rule = postcss.rule({selector})
-        const style = func({[prop]: key})
-        for (const [property, value] of Object.entries(style)) {
-          // skip over nested scales
-          if (value && typeof value === 'object') {
-            continue
-          }
-          const cssValue = px(value)
-          const decl = postcss.decl({
-            prop: hyphenate(property),
-            value: cssValue,
-            important: true
+      for (const [scaleKey, scaleValue] of Object.entries(scale)) {
+        const keys = getNestedKeys(scaleKey, scaleValue)
+        for (const key of keys) {
+          const selector = getSelector({
+            prop: prefix || prop,
+            breakpoint,
+            value: key
           })
-          rule.append(decl)
+          const rule = postcss.rule({selector})
+          const style = func({[prop]: key, theme})
+          for (let [property, value] of Object.entries(style)) {
+            const cssValue = px(value)
+            const decl = postcss.decl({
+              prop: hyphenate(property),
+              value: cssValue,
+              important: true
+            })
+            rule.append(decl)
+          }
+          append(rule)
         }
-        append(rule)
       }
     }
   }
@@ -130,11 +156,12 @@ function generateRulesForStyle(systemProp, theme, breaks) {
   return rules
 }
 
-function getSelector({prop, breakpoint, key, value}) {
+function getSelector({prop, breakpoint, value}) {
   const parts = breakpoint
-    ? [prop, breakpoint, key]
-    : [prop, key]
-  return `.${parts.join('-')}`
+    ? [prop, breakpoint, value]
+    : [prop, value]
+  const className = parts.join('-').replace(/\./g, '-')
+  return `.${className}`
 }
 
 function getDefaultScale(prop, cssProperty) {
@@ -152,5 +179,35 @@ function num(n) {
 }
 
 function px(n) {
-  return num(n) ? `${n}px` : n
+  return num(n) && n ? `${n}px` : n
+}
+
+function joinKeys(...keys) {
+  return keys.join('.')
+}
+
+function isObject(d) {
+  return d && typeof d === 'object'
+}
+
+function getNestedKeys(prefix, values) {
+  if (isObject(values)) {
+    return Object.keys(values).reduce((keys, k) => {
+      const key = joinKeys(prefix, k)
+      keys.push(key)
+      const value = values[k]
+      if (isObject(value)) {
+        return keys.concat(
+          getNestedKeys(key, value)
+        )
+      }
+      return keys
+    }, [])
+  } else {
+    return [prefix]
+  }
+}
+
+function mapify(list) {
+  return list.reduce((d, v) => Object.assign(d, {[v]: v}), {})
 }
